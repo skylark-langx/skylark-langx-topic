1e170afb4c8cceba44ea53c01488cba5b6b8b5b4
hudaokeji <github@hudaokeji.com>
2020-12-13 01:21:59 +0900

    update

--- dist/skylark-langx-topic/topic.js
+++ dist/skylark-langx-topic/topic.js
@@ -5,5 +5,5 @@
  * @link www.skylarkjs.org
  * @license MIT
  */
-define(["skylark-langx-ns","skylark-langx-emitter/Evented"],function(n,t){var a=new t;return n.attach("langx.topic",{publish:function(n,t,e){var r=[].slice.call(arguments,1);return a.trigger({type:n,data:r})},subscribe:function(n,t,e){var r=function(n){t.apply(e,n.data)};return a.on(n,r),{remove:function(){a.off(n,r)}}}})});
+define(["skylark-langx-ns","skylark-langx-events"],function(n,t){var a=new t.Evented;return n.attach("langx.topic",{publish:function(n,t,e){var r=[].slice.call(arguments,1);return a.trigger({type:n,data:r})},subscribe:function(n,t,e){var r=function(n){t.apply(e,n.data)};return a.on(n,r),{remove:function(){a.off(n,r)}}}})});
 //# sourceMappingURL=sourcemaps/topic.js.map
